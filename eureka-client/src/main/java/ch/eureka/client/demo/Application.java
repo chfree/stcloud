b59eba64e85d23dd9d389cb65175cf451362b7b5
zfree <79763939@qq.com>
2019-06-24 12:49:03 +0800

    初始化代码-可运行的注册中心和服务注册

---
+++ eureka-client/src/main/java/ch/eureka/client/demo/Application.java
@@ -0,0 +1,13 @@
+package ch.eureka.client.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
